3278a73ce5e747a4e16cb415d042bad1d04021b8
Sisco-C <siscoronojey@gmail.com>
2021-07-25 01:00:01 +0300

    Edits

--- mysimpleapi.py
+++ mysimpleapi.py
@@ -1,3 +1,4 @@
+from os import name
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, DEFAULT_REPRESENTATIONS, Resource
@@ -5,69 +6,64 @@ from werkzeug.wrappers import Request
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///destination.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 
 db = SQLAlchemy(app)
 
 
-class Destination(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    serviceprovider = db.Column(db.String, unique=True, nullable=False)
-    residentrates = db.Column(db.Integer)
-    nonresidentrates = db.Column(db.String, nullable=False)
-    location = db.Column(db.String, nullable=False)
-    peakseasons = db.Column(db.String, nullable=False)
-    codetogetdiscount = db.Column(
-        db.String, unique=True, nullable=False)
-    agentcontact = db.Column(db.String(80), nullable=False)
+    firstname = db.Column(db.String, unique=True, nullable=False)
+    lastname = db.Column(db.String)
+    emailaddress = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"{self.serviceprovider} - {self.location} - {self.residentrates} - {self.nonresidentrates} - {self.agentcontact} - {self.peakseasons} - {self.codetogetdiscount}"
+        return f"{self.firstname} - {self.lastname} - {self.emailaddress} - {self.age} "
 
 
 @ app.route('/')
 def index():
 
     # api = Api(app)
-    return 'Hello user, Traverza here!'
+    return 'Hello user, Welcome here!'
 
 
-@ app.route('/destination')
-def get_destinations():
-    destinations = Destination.query.all()
+@ app.route('/user')
+def get_users():
+    users = User.query.all()
 
     output = []
-    for destination in destinations:
-        destination_data = {'service provider': destination.serviceprovider, 'resident rates': destination.residentrates, 'non resident rates': destination.nonresidentrates,
-                            'location': destination.location, 'peak seasons': destination.peakseasons, 'code to get discount': destination.codetogetdiscount, 'agent contact': destination.agentcontact}
+    for user in users:
+        user_data = {'first name': user.firstname, 'last name': user.lastname, 'email address': user.emailaddress,
+                     'age': user.age, }
 
-    output.append(destination_data)
-    return{"destinations": "output"}
-
-
-@app.route('/destinations/<id>')
-def get_destination(id):
-    destination = Destination.query.get_or_404(id)
-    return ({{"service provider": destination.serviceprovider, "resident rates": destination.residentrates, "non resident rates": destination.nonresidentrates,
-              "location": destination.location, "peak seasons": destination.peakseasons, "code to get discount": destination.codetogetdiscount, "agent contact": destination.agentcontact}})
+    output.append(user_data)
+    return{"users": "output"}
 
 
-@app.route('/destinations/', methods=['POST'])
-def add_destination():
-    destination = Destination(serviceprovider=request.json['service provider'], residentrates=request.json['resident rates'], nonresidentrates=request.json['non resident rates'],
-                              location=request.json['location'], codetogetdiscount=request.json['code to get discount'], agentcontact=request.json['agent contact'], peakseasons=request.json['peak seasons'],)
+@app.route('/users/<id>')
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return ({"first name": user.firstname, "last name": user.lastname, "email address": user.emailaddress,
+             "age": user.age, })
 
-    db.session.add(destination)
+
+@app.route('/users/', methods=['POST'])
+def add_user():
+    user = User(firstname=request.json['firstname'], lastname=request.json['lastname'], emailaddress=request.json['emailaddress '],
+                age=request.json['age'],)
+    db.session.add(user)
     db.session.commit()
-    return{'id': destination.id}
+    return{'id': user.id}
 
 
-@app.route('/destinations/<id>', method=['DELETE'])
-def delete_destination(id):
-    destination = Destination.query.get(id)
-    if destination is None:
-        return{"error": "destination not found"}
-    db.session.delete(destination)
+@app.route('/users/<id>', method=['DELETE'])
+def add_user(id):
+    user = User.query.get(id)
+    if user is None:
+        return{"error": "user not found"}
+    db.session.delete(user)
     db.session.commit()
     return {"Message": "{Deleted successfully!"}
